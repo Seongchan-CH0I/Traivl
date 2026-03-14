b625abb0444cc2c1f5cc544ad4377ecddaef428a
Seongchan-CHOI <chltjdcks329@naver.com>
2026-03-14 11:54:23 +0900

    fix: 병합 시 생긴 오류 해결_최종

--- front_backend/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL || "postgresql://postgres:password@127.0.0.1:5432/traivldb",
-  },
-});
