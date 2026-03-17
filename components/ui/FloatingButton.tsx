34ec4b74ef02e9827a12dccf54810162ef2e0ea6
yoon <yoonjusang@naver.com>
2026-03-17 11:19:51 +0900

    feat:인터그레이션과 구조 통일화

--- components/ui/FloatingButton.tsx
+++
@@ -1,9 +0,0 @@
-export default function FloatingButton({ onClick }: { onClick?: () => void }) {
-    return (
-        <div className="floating-action-btn-container">
-            <button className="floating-action-btn" onClick={onClick}>
-                ✈️ 여행가기
-            </button>
-        </div>
-    );
-}
