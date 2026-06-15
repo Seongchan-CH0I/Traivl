b73daa75a7765252aa2438e0ab61772a18e1bd59
yoon <yoonjusang@naver.com>
2026-06-15 18:05:57 +0900

    chore: ignore and remove brain directory from git

--- brain/bab81d6a-d86b-4337-8754-768e66f33d58/scratch/check_tripadvisor.py
+++
@@ -1,6 +0,0 @@
-with open(r'c:\Traivl\Traivl\database\init.sql', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if 'dynamic-media-cdn' in line:
-        print(f"Line {i+1}: {line[:150]}...")
